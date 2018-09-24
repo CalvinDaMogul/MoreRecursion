1fa9318c5f2bc1ca3a8cd50bf3a64fa0c61d6f68
Calvin J Foster <cal.j.foster@gmail.com>
2018-09-23 20:54:23 -0500

    Test

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace fiboapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program me= new Program();
+            int f= me.fibo(1,2);
+
+            
+        }
+
+        public int fibo(int a, int b ) {
+            int math;
+
+           do{ 
+               math=a+b;
+            Console.WriteLine(a+"+"+b+"="+math);               
+               
+
+            if(math%2>0) {
+                a=b;
+                b=math;
+                
+            }
+           }
+            while (math%2>0); 
+            return math;
+            
+             
+        }
+    }
+}
